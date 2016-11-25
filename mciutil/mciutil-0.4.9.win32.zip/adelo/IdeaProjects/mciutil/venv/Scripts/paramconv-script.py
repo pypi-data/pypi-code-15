c354d31444ebd4f1901f624bb0c23bde16257b2f
Tom Forbes <tom@tomforb.es>
2016-11-25 11:53:20 +0000

    {"name":"mciutil","version":"0.4.9","file":"mciutil-0.4.9.win32.zip","path":"mciutil/mciutil-0.4.9.win32.zip"}

---
+++ mciutil/mciutil-0.4.9.win32.zip/adelo/IdeaProjects/mciutil/venv/Scripts/paramconv-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\adelo\IdeaProjects\mciutil\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'mciutil==0.4.9','console_scripts','paramconv'
+__requires__ = 'mciutil==0.4.9'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('mciutil==0.4.9', 'console_scripts', 'paramconv')()
+    )
